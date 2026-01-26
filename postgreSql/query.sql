caa1e7875283d812eb38737ca20c139d1e55d724
sangsil <sangsil9866@gmail.com>
2026-01-27 00:02:36 +0900

    postgresql 정리

--- postgreSql/query.sql
+++ postgreSql/query.sql
@@ -1,10 +1,3 @@
--- 0. 관리자로 접속
-docker exec -it postgres psql -U dbuser -d dbname
-
--- 1. gitea라는 유저 생성 (비밀번호는 원하는대로, 여기선 gitea1234)
-CREATE USER gitea WITH PASSWORD 'gitea1234';
-
--- 2. gitea라는 이름의 데이터베이스 생성하고 주인은 gitea로 설정
-CREATE DATABASE gitea OWNER gitea;
--- 3. 나가기
-\q
+-- 3. silServer 프로젝트용 스키마 생성 (소유권 부여)
+CREATE SCHEMA silserver AUTHORIZATION dbuser;
+GRANT ALL ON SCHEMA silserver TO dbuser;
